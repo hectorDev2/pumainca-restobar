ffbe04030d156dae37d8eefc1753e522fbcec788
Hector Barazorda <ph.barazorda@gmail.com>
2026-01-16 08:48:00 -0500

    feat: reorganizar Providers para incluir AuthProvider como contenedor principal

--- lib/Providers.tsx
+++ lib/Providers.tsx
@@ -4,16 +4,19 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { CartProvider } from "@/context/CartContext";
+import { AuthProvider } from "@/context/AuthContext";
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <CartProvider>
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} />
-      </CartProvider>
+      <AuthProvider>
+        <CartProvider>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </CartProvider>
+      </AuthProvider>
     </QueryClientProvider>
   );
 }
