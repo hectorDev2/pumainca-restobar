a654b1eeef643587c61be678a79f9bb42cc55b06
Hector Barazorda <ph.barazorda@gmail.com>
2026-01-19 12:23:13 -0500

    style: Enhance global theming with dynamic CSS variables for light and dark modes, improving background and text color management across components for better readability and user experience.

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" className="mdl-js">
+    <html lang="es" className="mdl-js dark">
       <body className={inter.className}>
         <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Noto+Sans:wght@300..800&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
