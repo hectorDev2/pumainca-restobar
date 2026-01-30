46e2763165b6d2c15c86f4ad60cd6ab0e1f1e2e9
Hector Barazorda <ph.barazorda@gmail.com>
2026-01-30 17:50:52 -0500

    feat: Agregar índice de componentes de inicio y mejorar re-exportaciones en componentes UI

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -3,13 +3,22 @@
  * Re-exports all UI components for easier imports
  */
 
-export { default as AdminModal } from "./admin-modal";
-export { default as AnimatedModal } from "./animated-modal";
+export { AdminModal } from "./admin-modal";
 export { default as AnimatedModalDemo } from "./animated-modal-demo";
-export { default as AnimatedThemeToggler } from "./animated-theme-toggler";
+export {
+  ModalProvider,
+  useModal,
+  Modal,
+  ModalTrigger,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  useOutsideClick,
+} from "./animated-modal";
+export { AnimatedThemeToggler } from "./animated-theme-toggler";
 export { default as Carousel3D } from "./carousel-3d";
-export { default as FileUpload } from "./file-upload";
-export { default as GlareCard } from "./glare-card";
-export { default as Loader } from "./loader";
-export { default as MenuImage } from "./menu-image";
-export { default as NeonGradientCard } from "./neon-gradient-card";
+export { FileUpload } from "./file-upload";
+export { GlareCard } from "./glare-card";
+export { Loader } from "./loader";
+export { MenuImage } from "./menu-image";
+export { NeonGradientCard } from "./neon-gradient-card";
